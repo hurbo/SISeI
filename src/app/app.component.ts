f0fb9c6314d737a1ececb379aa368c5280bac388
Alfonso Torres Echave <hurbogt522o@gmail.com>
2017-10-25 14:25:24 -0600

    agregar inicio de sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user: Observable<firebase.User>;
+  // items: FirebaseListObservable<any[]>;
+
+  msgVal: string = '';
+
   title = 'app';
+  items = ['1','2','3'];
+
+
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.user = this.afAuth.authState;
+
+    console.log('user ', this.user);
+  }
+
+
+
+  login() {
+    this.afAuth.auth.signInAnonymously();
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  Send(desc: string) {
+    console.log('send ',desc);
+    this.msgVal = '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Import Forms
+import { FormsModule } from '@angular/forms';
+
+// Import Http
+import { HttpModule } from '@angular/http';
+
+
 // Import Angular plugin.
 import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+
+// Import AngularFire2
+import { AngularFireModule } from 'angularfire2';
+// New imports to update based on AngularFire2 version 4, database and auth
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLVg7bCMEiRG7xD4eu4_sv5G6BqAd1JUU",
+  authDomain: "go-angular-f9e0f.firebaseapp.com",
+  databaseURL: "https://go-angular-f9e0f.firebaseio.com",
+  projectId: "go-angular-f9e0f",
+  storageBucket: "go-angular-f9e0f.appspot.com",
+  messagingSenderId: "387596480886"
+};
 
 @NgModule({
   declarations: [
@@ -13,7 +37,12 @@ import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
   imports: [
     BrowserModule,
     FroalaEditorModule.forRoot(),
-    FroalaViewModule.forRoot()
+    FroalaViewModule.forRoot(),
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
