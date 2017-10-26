5f479c07e1d1fe0c9231f648ed6e5f8101677faa
Alfonso Torres Echave <hurbogt522o@gmail.com>
2017-10-26 15:15:47 -0600

    fix input

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Output} from '@angular/core';
 
 @Component({
   selector: 'profile-main',
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   title = 'Panel de adinistración';
+  nombreDelPapa : string;
 
-  constructor() { }
+  constructor() {
+    this.nombreDelPapa = 'Inicializado desde constructor del padre';
+  }
 
   ngOnInit() {
   }
